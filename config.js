e9d5cde7cd9ee806f4e3df676dd12ea56c017fd6
Qtram98 <88839665+Qtram98@users.noreply.github.com>
2021-10-19 23:41:37 +0700

    upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Dear Bae, Hoàng Nhật Ân!',
+    introDesc: ` Hôm nay là ngày 20/10/2021- tròn 2 năm bên nhau nè!`,
+    btnIntro: 'Love diu',
+    title: 'Cảm ơn đã đến bên em',
+    desc: 'Thấy 2 năm dã man ko??? ',
+    btnYes: 'CÓ, DÃ MAN VKL <33',
+    btnNo: 'KO, Yêu nhiều lắmmmmm',
+    question:'Ủa ???? Lí do <3',
+    btnReply: 'Gửi cho Honey <3',
+    reply: 'Dữ dằn hay hờn dỗi, vô lí, cãi cùn nhứttt <33333333',
+    mess: 'Dữ dằn con mắt anh. Cảm ơn anh đã chịu đựng và yêu e suốt thời gian qua, hi vọng cùng nhau bước chặng đường dài 😘😘',
+    messDesc: 'Lựa lời mà nói nghe .',
+    btnAccept: 'Dạ, miễn là có e bên cạnh hihi <3',
+    messLink: 'https://www.facebook.com/quynhtram.huynhthi' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
